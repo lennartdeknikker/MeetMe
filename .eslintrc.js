e8326a2bab591b9b8c363ca073956add0c929181
lennartdeknikker <me@lenn4rt.com>
2019-06-23 15:26:23 +0200

    :wrench: add .editorconfig and .eslintrc.js files

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true,
+        "mongo": true,
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"],
+        "no-console": "off",
+    }
+};
