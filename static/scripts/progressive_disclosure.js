485836b876d450e61e355f021079996842a46711
lennartdeknikker <me@lenn4rt.com>
2019-06-24 14:45:36 +0200

    :alembic: add a progress-indicator to the progressive disclosure when changing information

--- static/scripts/progressive_disclosure.js
+++ static/scripts/progressive_disclosure.js
@@ -1,18 +1,19 @@
 let fieldsets = document.getElementsByTagName('fieldset');
 let form = document.getElementById('form-profile');
+let headertext = document.getElementsByTagName('h1');
 
 let progress = 0
 
 hideOtherFieldsets(progress);
 
 // creates a next button.
-newButton = document.createElement('button');
-newButton.innerHTML = "next";
-newButton.classList.add('save-button')
-newButton.addEventListener('click', function () {
+let nextButton = document.createElement('button');
+nextButton.innerHTML = "next";
+nextButton.classList.add('save-button');
+nextButton.addEventListener('click', function () {
     hideOtherFieldsets(progress)
 });
-form.parentNode.append(newButton);
+form.parentNode.append(nextButton);
 
 /*  This function is called everytime the next button is clicked.
     It hides the other fieldsets and makes the current one visible based on an incremental progress variable.
@@ -21,13 +22,15 @@ function hideOtherFieldsets(currentFieldset) {
     for (i = 0; i < fieldsets.length; i++) {
         if (i == currentFieldset) {
             fieldsets[i].style.display = 'block';
+            let currentText = fieldsets[i].childNodes[1].innerHTML;
+            fieldsets[i].childNodes[1].innerHTML = ` <span class='progress-indicator'>${progress+1}/${fieldsets.length-1}:</span> ${currentText}`;
         } else {
             fieldsets[i].style.display = 'none';
         }
     }
     if (currentFieldset > fieldsets.length - 3) {
         fieldsets[fieldsets.length - 1].style.display = 'block';
-        form.parentNode.removeChild(newButton);
+        form.parentNode.removeChild(nextButton);
     }
 
     progress++;
