523c01f2422d09688763757e5e726ad4acae0a8c
lennartdeknikker <me@lenn4rt.com>
2019-06-24 15:00:18 +0200

    :pencil2: clean up progressive disclosure script

--- static/scripts/progressive_disclosure.js
+++ static/scripts/progressive_disclosure.js
@@ -1,17 +1,16 @@
-let fieldsets = document.getElementsByTagName('fieldset');
-let form = document.getElementById('form-profile');
-let headertext = document.getElementsByTagName('h1');
+let fieldsets = document.getElementsByTagName("fieldset");
+let form = document.getElementById("form-profile");
 
-let progress = 0
+let progress = 0;
 
 hideOtherFieldsets(progress);
 
 // creates a next button.
-let nextButton = document.createElement('button');
+let nextButton = document.createElement("button");
 nextButton.innerHTML = "next";
-nextButton.classList.add('save-button');
-nextButton.addEventListener('click', function () {
-    hideOtherFieldsets(progress)
+nextButton.classList.add("save-button");
+nextButton.addEventListener("click", function () {
+	hideOtherFieldsets(progress);
 });
 form.parentNode.append(nextButton);
 
@@ -19,19 +18,20 @@ form.parentNode.append(nextButton);
     It hides the other fieldsets and makes the current one visible based on an incremental progress variable.
 */
 function hideOtherFieldsets(currentFieldset) {
-    for (i = 0; i < fieldsets.length; i++) {
-        if (i == currentFieldset) {
-            fieldsets[i].style.display = 'block';
-            let currentText = fieldsets[i].childNodes[1].innerHTML;
-            fieldsets[i].childNodes[1].innerHTML = ` <span class='progress-indicator'>${progress+1}/${fieldsets.length-1}:</span> ${currentText}`;
-        } else {
-            fieldsets[i].style.display = 'none';
-        }
-    }
-    if (currentFieldset > fieldsets.length - 3) {
-        fieldsets[fieldsets.length - 1].style.display = 'block';
-        form.parentNode.removeChild(nextButton);
-    }
+	for (let i = 0; i < fieldsets.length; i++) {
+		if (i == currentFieldset) {
+			fieldsets[i].style.display = "block";
+			let currentText = fieldsets[i].childNodes[1].innerHTML;
+			fieldsets[i].childNodes[1].innerHTML =
+				` <span class="progress-indicator">${progress+1}/${fieldsets.length-1}:</span> ${currentText}`;
+		} else {
+			fieldsets[i].style.display = "none";
+		}
+	}
+	if (currentFieldset > fieldsets.length - 3) {
+		fieldsets[fieldsets.length - 1].style.display = "block";
+		form.parentNode.removeChild(nextButton);
+	}
 
-    progress++;
+	progress++;
 }
